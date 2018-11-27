a7ccafbbdc9c65f863ec66db1280d539c0505b38
DESKTOP-GDM5KI7\Mreow <Sevarish@hotmail.com>
2018-11-27 11:06:35 +0100

    Added first version of Camera Movement

---
+++ Assets/Scripts/Jelle/Camera/TempCameraMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempCameraMovement : MonoBehaviour {
+    private Transform playerTransform;
+    private Transform enemyTransform;
+    private Transform cameraTransform;
+    [SerializeField]
+    private string playerName = "Temp_Player";
+    [SerializeField]
+    private string enemyName = "Temp_Opponent";
+    private Camera cam;
+    private float camMove;
+    private bool isCentered = true;
+	void Start () {
+        playerTransform = GameObject.Find(playerName).GetComponent<Transform>();
+        enemyTransform = GameObject.Find(enemyName).GetComponent<Transform>();
+        cameraTransform = GetComponent<Transform>();
+        cam = GetComponent<Camera>();
+	}
+	
+	void Update () {
+        KeepPlayerInView();
+    }
+
+    private Vector3 CalculateCenter()
+    {
+        Vector3 result = (playerTransform.position + enemyTransform.position) / 2;
+        return result;
+    }
+
+    private Vector3 ObjectDistanceFromCamEdge()
+    {
+        Vector3 screenPos = cam.WorldToScreenPoint(playerTransform.position);
+        return screenPos;
+    }
+
+    private void KeepPlayerInView()
+    {
+        if (ObjectDistanceFromCamEdge().x < 450)
+        {
+            camMove = -6.2f;
+        }
+        else if (ObjectDistanceFromCamEdge().x > 1500)
+        {
+            camMove = 6.2f;
+        }
+        else
+        {
+            if (ObjectDistanceFromCamEdge().x > 950 && ObjectDistanceFromCamEdge().x < 970)
+            {
+                isCentered = true;
+            }
+            else
+            {
+                isCentered = false;
+            }
+            if (ObjectDistanceFromCamEdge().x > 450 && ObjectDistanceFromCamEdge().x < 960 && !isCentered)
+            {
+                camMove = -3.2f;
+            }
+            else if (ObjectDistanceFromCamEdge().x < 1500 && ObjectDistanceFromCamEdge().x > 960 && !isCentered)
+            {
+                camMove = 3.2f;
+            }
+            else
+            {
+                camMove = 0;
+            }
+        }
+        Debug.Log(ObjectDistanceFromCamEdge());
+        //cameraTransform.RotateAround(CalculateCenter(), new Vector3(0, 1, 0), camRot);
+        cameraTransform.Translate(camMove * Time.deltaTime, 0, 0);
+
+    }
+}
---
+++ Assets/Scripts/Jelle/Player/TempPlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempPlayerMovement : MonoBehaviour {
+    private Transform playerT;
+    private float playerMoveSpeed;
+	// Use this for initialization
+	void Start () {
+        playerT = GetComponent<Transform>();
+        playerMoveSpeed = 6.7f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Input
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerT.Translate(0,0, playerMoveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerT.Translate(0, 0, -playerMoveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerT.Translate(playerMoveSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerT.Translate(-playerMoveSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+}
