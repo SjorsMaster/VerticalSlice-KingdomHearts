646a66812e0d298277870a238c75c1bf5623a3af
DESKTOP-GDM5KI7\Mreow <Sevarish@hotmail.com>
2018-12-04 12:40:20 +0100

    Implemented animation and other small things

---
+++ Assets/Scripts/Jelle/Player/AnimationManagerPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManagerPlayer : MonoBehaviour {
+    private Animator playerAnim;
+    private readonly string playerRun = "Running";
+    private TempPlayerMovement scriptRef;
+	private void Start () {
+        playerAnim = GetComponent<Animator>();
+        scriptRef = GameObject.Find("Temp_Player").GetComponent<TempPlayerMovement>();
+	}
+	
+	private void Update () {
+        RotateAnimation();
+        PlayerRun();
+	}
+
+    private void PlayerRun()
+    {
+        playerAnim.Play(playerRun);
+    }
+
+    private void RotateAnimation()
+    {
+        if (scriptRef.facingDirection == scriptRef.faces[0])
+        {
+        }
+        else if (scriptRef.facingDirection == scriptRef.faces[1])
+        {
+        }
+        else if (scriptRef.facingDirection == scriptRef.faces[2])
+        {
+        }
+        else if (scriptRef.facingDirection == scriptRef.faces[3])
+        {
+        }
+
+    }
+}
---
+++ Assets/Scripts/Shailesh/Enemy/SpawnBall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBall : MonoBehaviour {
+
+    private GameObject ObjectToAcces;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        ObjectToAcces = GameObject.Find("BallSpawner");
+        InstantiateBall scriptToAccess = ObjectToAcces.GetComponent<InstantiateBall>();
+        scriptToAccess.Spawn(0,0,0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
